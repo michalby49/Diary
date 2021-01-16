cfea74ef21e9502b4c52cee99f9c749aca5176c8
michalby49 <beskamichal49@gmail.com>
2021-01-16 11:44:00 +0100

    BazyDanych

--- Diary/Model/Domains/Collection.cs
+++ Diary/Model/Domains/Collection.cs
@@ -1,8 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Diary.Model.Domains
 {
     internal class Colletion<T> : ICollection<Rating>
     {
+        public int Count => throw new System.NotImplementedException();
+
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add(Rating item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(Rating item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(Rating[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<Rating> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(Rating item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
